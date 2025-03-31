c6190bc71a6377221d3dde649b65cdf20034f371
ajjurathod <131947107+ajjurathod@users.noreply.github.com>
2025-03-31 12:57:10 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
 def call(){
-  sh "docker build -t ajay-imge ."
-  sh "docekr run -td --name ajay-cont -p 80:80 ajay-image"
+  sh "sudo docker build -t ajay-imge ."
+  sh "sudo docekr run -td --name ajay-cont -p 80:80 ajay-image"
 }
